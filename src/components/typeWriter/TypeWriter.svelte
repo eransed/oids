e82ac3e39c375d2bdff63f3529dfb3bd97a55423
Aheige <aheige17@gmail.com>
2023-05-26 08:08:04 +0200

    Typewriter now supports human randomeness in typing and choosable if iterative delete letters from message after its done.

--- src/components/typeWriter/TypeWriter.svelte
+++ src/components/typeWriter/TypeWriter.svelte
@@ -4,20 +4,44 @@
 
   export let text: string
   export let speed: number = 100
+  export let humanRandomeness: boolean = false
+  export let deleteMessage: boolean = false
 
   let i = 0
   let typeWriterText: string = ""
-
-  console.log("what")
+  let finished: boolean = false
 
   const typeWriter = (): void => {
-    setTimeout(() => {
-      if (i < text.length) {
-        typeWriterText += text.charAt(i)
-        i++
-        typeWriter()
-      }
-    }, speed)
+    //This is the typewriter. For every 'pass' it puts a new letter in variable typeWriterText
+    //which outputs in the html.
+    if (!finished) {
+      setTimeout(() => {
+        if (i < text.length) {
+          typeWriterText += text.charAt(i)
+          i++
+          if (i === text.length) {
+            finished = true
+            if (!deleteMessage) return
+          }
+          typeWriter()
+        }
+      }, speed * (humanRandomeness ? rndi(0.5, 2) : 1))
+    }
+
+    //Deleting message after message is finished and deleteMessage is true
+    //One letter at a time
+    if (finished && deleteMessage) {
+      setTimeout(() => {
+        if (i > 0) {
+          typeWriterText = typeWriterText.slice(0, -1)
+          i--
+          if (i === 0) {
+            return
+          }
+          typeWriter()
+        }
+      }, speed * 0.5)
+    }
   }
 
   typeWriter()
