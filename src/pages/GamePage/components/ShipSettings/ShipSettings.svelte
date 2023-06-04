ebcd64e516046d91e4b91dbf4ffb308843f9a52d
eransed <emil.ransed@gmail.com>
2023-06-05 00:27:54 +0200

    ship settings object prop count of array length

--- src/pages/GamePage/components/ShipSettings/ShipSettings.svelte
+++ src/pages/GamePage/components/ShipSettings/ShipSettings.svelte
@@ -2,16 +2,25 @@
   import { localPlayerStore } from "../Game/Utils/gameUtils"
 
   function prettySpaceObjectValue(value: any): string {
+    // let out: string = ':' + (typeof value) + ' = '
+    const valType = (typeof value)
+    let out = ''
     if (typeof value === 'object') {
-      let out = ''
-
       Object.entries(value).forEach((v) => {
         out += `${v[0]}: ${prettySpaceObjectValue(v[1])}, `
       })
       out = out.substring(0, out.length - 2)
       return out
     }
-    return value
+    return out + value
+  }
+
+  function getObjectPropCountIfAny(obj: any): string {
+    if (typeof obj === 'object') {
+      const propCount = Object.keys(obj).length
+      if (propCount > 0) return `[${propCount}]`
+    }
+    return ''
   }
 
 </script>
@@ -38,5 +47,5 @@
 
 <h6>{$localPlayerStore.name} [{Object.keys($localPlayerStore).length}]</h6>
 {#each Object.entries($localPlayerStore) as kv, index}
-  <div><span style="color: {$localPlayerStore.color}">[{index + 1}].{kv[0]}</span>: {prettySpaceObjectValue(kv[1])}</div>
+  <div><span style="color: {$localPlayerStore.color}">[{index + 1}].{kv[0]}</span>{getObjectPropCountIfAny(kv[1])} = {prettySpaceObjectValue(kv[1])}</div>
 {/each}
